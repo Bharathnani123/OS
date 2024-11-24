1fac64637c757704a85be9818161addcd05e57fb
Bharathnani123 <124094604+Bharathnani123@users.noreply.github.com>
2024-11-25 00:28:19 +0530

    Create orphan_process.c

---
+++ orphan_process.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+	int pid;
+	pid = fork();
+	if(pid == 0)
+	{
+		printf("I am the child, my process ID is %d\n",getpid());
+		printf("My parent's process ID is %d\n",getppid());
+		sleep(5);
+		printf("\nAfter sleep\nI am the child, my process ID is %d\n",getpid());
+                printf("My parent's process ID is %d\n",getppid());
+		exit(0);
+	}
+	else
+	{
+		sleep(10);
+		printf("I am the parent, my process ID is %d\n",getpid());
+                printf("The parent's parent, process ID is %d\n",getppid());
+		printf("Parent terminates\n");
+	}
+	return 0;
